6e23b2793c8d0ab2ccce5576a8465ec1dbba3362
betogls <betogls02@gmail.com>
2018-02-03 06:59:18 -0700

    using constants

--- marvel_class.rb
+++ marvel_class.rb
@@ -6,11 +6,11 @@ require 'digest'
 class Marvel
   include HTTParty
   include SecureRandom
-  ts=SecureRandom.base64
-  apikey="98dff8d8fcd1870e5fe5ba9cc0176070"
+  TS=SecureRandom.base64
+  APIKEY="98dff8d8fcd1870e5fe5ba9cc0176070"
   #fix hash
-  #hash= Digest::MD5.hexdigest ""
-  @string="ts=#{ts}&apikey=#{apikey}&hash=#{hash}"
+  #HASH= Digest::MD5.hexdigest ""
+  STRING="ts=#{TS}&apikey=#{APIKEY}&hash=#{HASH}"
   base_uri "http://gateway.marvel.com/v1"
   attr_accessor :name
   attr_reader :id, :description
@@ -20,7 +20,7 @@ class Marvel
     @description = description
   end
   def self.find(name)
-    response = get("/public/characters?name=#{name}&"+ @string)
+    response = get("/public/characters?name=#{name}&"+ STRING)
     if response.success?
 	data = response["data"]
 	results = data["results"]
